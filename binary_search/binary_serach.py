fc7be0a674e47fa6be2de3fa83bd1275cf9221db
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-03-16 12:50:00 +0300

    Binary Search Implemented Using More Modern Way

--- binary_search/binary_serach.py
+++ binary_search/binary_serach.py
@@ -14,13 +14,32 @@ def binary_search_v1(arr, target):
     return -1
 
 
+def binary_search_v2(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while high > low + 1:
+        middle = (low + high) // 2
+        if arr[middle] == target:
+            return middle
+        elif arr[middle] > target:
+            high = middle
+        else:
+            low = middle
+    if arr[low] == target:
+        return low
+    elif arr[high] == target:
+        return high
+    else:
+        return -1
+
+
 if __name__ == "__main__":
     input("Please Enter Sorted List Of Element 'Press Enter To Start Entering Number'")
     arr = sorted(list(map(int, input().split())))
     
     target = int(input())
     
-    if binary_search_v1(arr, target) == -1:
+    if binary_search_v2(arr, target) == -1:
         print("Element not Found !!")
     else:
-        print("Element Found At : ", binary_search_v1(arr, target), " Index")
+        print("Element Found At : ", binary_search_v2(arr, target), " Index")
