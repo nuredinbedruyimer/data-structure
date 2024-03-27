30367544d0b27625fbc054fbce1c3279306e305d
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-03-27 12:00:00 +0300

    'C++' Struct

--- cp/c_learning/hello.cpp
+++ cp/c_learning/hello.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+struct node{
+    int index;
+    double weight;
+    bool isRemoved;
+    string name;
+
+    node(int index, double weight,bool isRemoved, string name){
+       this -> index = index;
+       this -> weight = weight;
+       this -> isRemoved = isRemoved;
+       this -> name = name;
+    }
+};
 
 int main() {
     cout << "Hello, World from C++!" << endl;
     int age = 12;
     double height = 123.3;
-    cout<<"Height And Age Product Become : " << height * age <<endl;
+    node n =   node(12, 12.2, true, "Abebe");
+    cout<<"Height And Age Product Become : " << n.index <<endl;
+    
+
     return 0;
 }
