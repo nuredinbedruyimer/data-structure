eee85b6aaebbbe95f9b2e542776c78dec9fb78a9
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-03-27 12:00:00 +0300

    'C++' set up is done

---
+++ cp/c_learning/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World from C++!" << endl;
+    int age = 12;
+    double height = 123.3;
+    cout<<"Height And Age Product Become : " << height * age <<endl;
+    return 0;
+}
